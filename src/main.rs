bfc6934aa5e5c5c3aa4fee77460ea622f493e087
Paul Ulerick <p.ulerick17@gmail.com>
2023-03-01 22:09:16 -0500

    got rid of old code and started fresh

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,77 @@
-mod parser;
+use std::env;
+use std::fs;
+use std::process::exit;
+use std::io::{self, BufRead, Write};
+
+mod scanner;
+use crate::scanner::*;
+
+fn run_file(path: &str) ->Result<(), String> {
+    // Reads program file
+    match fs::read_to_string(path) {
+        Err(msg) => return Err(msg.to_string()),
+        Ok(contents) => return run(&contents),
+    } 
+}
+
+fn run(contents: &str) -> Result<(), String> {
+    let scanner = Scanner::new(contents);
+    let tokens = scanner.scan_tokens()?;
+
+    for token in tokens {
+        println!("{:?}", token);
+    }
+    return Ok(());
+}
+
+// Basic REPL without the evaluation
+fn run_prompt() -> Result<(), String> {
+    loop {
+        print!("> ");
+        match io::stdout().flush() {
+            Ok(_) => (),
+            Err(_) => return Err("Could not flush Stdout".to_string()),
+        }
+
+        let mut input = String::new();
+        match io::stdin().lock().read_line(&mut input) {
+            Ok(n) => {
+                if n <= 1 {
+                    return Ok(());
+                }
+            },
+            Err(_) => return Err("Couldn't read the line".to_string()),
+        }
+            
+        println!("{}", input);
+        match run(&input) {
+            Ok(_) => (),
+            Err(msg) => println!("{}", msg),
+        }
+    }
+}
 
 fn main() {
-    let string = String::from("8");
-    parser::parse_data(&string);
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 2 {
+        println!("Usage: Symphony [script]");
+        exit(64);
+    } else if args.len() == 2 {
+        match run_file(&args[1]) {
+            Ok(_) => exit(0),
+            Err(msg) => {
+                println!("Error:\n{}", msg);
+                exit(1);
+            }
+        }
+    } else {
+        match run_prompt() {
+            Ok(_) => exit(0),
+            Err(msg) => {
+                println!("Error:\n{}", msg);
+                exit(1);
+            }
+        }
+    }
 }
